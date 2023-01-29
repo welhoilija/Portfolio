87dc2a1b4e974fbbb7966c39fe761248f460b485
welhoilija <tkankas@gmail.com>
2023-01-29 19:53:51 +0200

    update static_root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,6 +12,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
