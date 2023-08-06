cfcfa2d58038b4d454fcb663cf7946cf501ec45e
welhoilija <tkankas@gmail.com>
2023-08-06 19:35:46 +0300

    Create endpoint for generating a seed based on request information

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.BlogView.as_view(), name="BlogView"),
     path("Author", views.AuthorView.as_view(), name="AuthorView"),
     path("Contact", views.ContactView.as_view(), name="ContactView"),
+    path('generate', views.SeedGeneratorView.as_view(), name='GenerateSeedView'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from hashlib import sha256
+
 from rest_framework import generics, status
-from .serializers import BlogSerializer, AuthorSerializer, ContactSerializer
-from .models import Blog, Author, ContactData
-from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here
+from rest_framework.views import APIView
+
+from .models import Author, Blog, ContactData
+from .serializers import AuthorSerializer, BlogSerializer, ContactSerializer
 
 
 class BlogView(generics.ListAPIView):
@@ -21,14 +22,26 @@ class ContactView(APIView):
     serializer_class = ContactSerializer
 
     def post(self, request, format=None):
-        user = request.user
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             text = serializer.data.get("text")
             name = serializer.data.get("name")
-            obj, created = ContactData.objects.update_or_create(
+            obj, _ = ContactData.objects.update_or_create(
                 text=text, name=name)
             obj.send_data_via_email()
 
         return Response(ContactSerializer(obj).data, status=status.HTTP_201_CREATED)
-        pass
+
+
+class SeedGeneratorView(APIView):
+    # generate unique sha256 seed for the user using website
+    def generate_seed(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+        addr = request.META.get('REMOTE_ADDR', '')
+        unique_string = f"{user_agent}-{addr}".encode('utf-8')
+        seed = sha256(unique_string).hexdigest()
+        return seed
+
+    def post(self, request, *args, **kwargs):
+        seed = self.generate_seed(request)
+        return Response({'seed': seed})
