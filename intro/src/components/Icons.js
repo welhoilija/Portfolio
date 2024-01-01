010be727aa612f44656fe059c4da78fe00454470
Tuomas Kangas <tkankas@gmail.com>
2024-01-01 18:15:15 +0200

    Format icons

--- intro/src/components/Icons.js
+++ intro/src/components/Icons.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// LinkedIn Icon Component
 export const LinkedInIcon = ({ className }) => (
   <svg
     role="img"
@@ -13,6 +12,7 @@ export const LinkedInIcon = ({ className }) => (
     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
   </svg>
 );
+
 export const TelegramIcon = ({ className }) => (
   <svg
     role="img"
@@ -25,6 +25,7 @@ export const TelegramIcon = ({ className }) => (
     <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.020.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
   </svg>
 );
+
 export const GitHubIcon = ({ className }) => (
   <svg
     role="img"
@@ -40,6 +41,7 @@ export const GitHubIcon = ({ className }) => (
     />
   </svg>
 );
+
 export const MenuIcon = ({ className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -56,6 +58,7 @@ export const MenuIcon = ({ className }) => (
     />
   </svg>
 );
+
 export const DashBoardIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -68,6 +71,7 @@ export const DashBoardIcon = () => (
     <path d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z"></path>
   </svg>
 );
+
 export const AboutMeIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -80,6 +84,7 @@ export const AboutMeIcon = () => (
     <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
   </svg>
 );
+
 export const ProjectsIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
